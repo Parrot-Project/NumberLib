70b771984147eabd507f3c57b7e9d8689b756957
ParrotCoder <wangyihandw@gmail.com>
2024-11-20 10:24:54 +0800

    update big_number.rs

--- src/big_number.rs
+++ src/big_number.rs
@@ -1,21 +1,58 @@
 pub struct Bignum {
-    digits: Vec<u8>
+    digits: Vec<u8>,
+    trailing_zeros: usize,
 }
 
 impl Bignum {
-    pub fn new(number: &str) -> Self {
+    pub fn new<T: ToString>(number: T) -> Self {
+        // 转换为string类型
+        let number_str = number.to_string();
+        
+        // 去除前导零
+        let trimmed_str = number_str.trim_start_matches('0');
+        
+        // 计算后导零的数量
+        let mut trailing_zeros = number_str.len() - trimmed_str.len();
+        
+        // 将去除前导零后的字符串转换为 Vec<u8>
+        let mut digits: Vec<u8> = trimmed_str.chars()      // 把字符串转换为字符
+            .filter_map(|c| c.to_digit(10))// 把字符转换为数字
+            .map(|n| n as u8)                     // 把u32转换为u8
+            .collect(); // 存储到vec中
+        
+        // 处理特殊情况：如果数字全是零
+        if digits.is_empty() {
+            digits = vec![0];  // 数字全是零时，表示为 [0]
+            trailing_zeros = 0;
+        }
+        
         Bignum {
-            digits: number.chars()            // 遍历字符串中的字符
-                .filter_map(|c| c.to_digit(10))   // 转换每个字符为数字（如果是数字）
-                .map(|d| d as u8)                 // 将数字转换为 u8 类型
-                .collect::<Vec<u8>>()           // 收集到一个 Vec<u8>,
+            digits,
+            trailing_zeros,
         }
     }
+
+    // 获取数字的有效位数
+    pub fn valid_digits(&self) -> &Vec<u8> {
+        &self.digits
+    }
+
+    // 获取后导零的数量
+    pub fn trailing_zeros(&self) -> usize {
+        self.trailing_zeros
+    }
+    
+    // 输出
     pub fn print(&self) {
-        print!("{}", self.digits.iter()
-                        .map(|n| n.to_string()) // 将每个数字转换为字符串
-                        .collect::<Vec<String>>() // 收集成一个 Vec<String>
-                        .join("") // 拼接为一个字符串
-              );
+        // 获取实际位的字符串
+        let mut result: String = self.digits.iter()
+            .map(|&digit| char::from_digit(digit as u32, 10).unwrap())
+            .collect();
+
+        // 添加后导零
+        result.push_str(&"0".repeat(self.trailing_zeros));
+
+        // 输出
+        println!("{}\n", result);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use number_lib::big_number::Bignum;
 
 fn main() {
-    let a = Bignum::new("12345");
+    let a = Bignum::new("012300");
     a.print();
-    let b = Bignum::new("654321");
+    let b = Bignum::new(321123);
     b.print();
+    let c = Bignum::new("0000000");
+    c.print();
+    println!("{}  {:?}", c.trailing_zeros(), c.valid_digits())
 }
